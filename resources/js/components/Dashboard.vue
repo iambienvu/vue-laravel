dc15712049b262b765d1631a05315526c74b4e5c
iambienvu <bienvq@wbcvn.vn>
2021-04-09 11:49:36 +0700

    first commit

---
+++ resources/js/components/Dashboard.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        Dashboard <br>
+        Name: {{user.name}}
+        Email: {{user.email}} <br>
+        <button @click.prevent="logout">Logout</button>
+    </div>
+</template>
+<script>
+    export default {
+        data(){
+            return {
+                user: null
+            }
+        },
+        mounted() {
+            axios.get('/api/user').then((res) =>{
+                this.user = res.data
+            })
+        },
+        methods: {
+            logout(){
+                axios.post('/api/logout').then(() =>{
+                    this.$alert("You have been logout successfully!");
+                    this.$router.push({ name: "Home" });
+                })
+            }
+        }
+    }
+</script>
